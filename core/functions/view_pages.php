b83a1e2f835184b4598e69c80038b7594b618220
killserver <killeserver@gmail.com>
2014-07-24 14:32:14 +0400

    delete not core

--- core/functions/view_pages.php
+++ core/functions/view_pages.php
@@ -18,83 +18,8 @@ global $manifest;
 		return;
 	}
 	switch($page) {
-		case "sitemap":
-			include_once(ROOT_PATH."core/pages/sitemap.".ROOT_EX);
-		break;
-		case "simular":
-			include_once(ROOT_PATH."core/pages/simular.".ROOT_EX);
-		break;
-		case "view":
+		case "news":
 			include_once(ROOT_PATH."core/pages/view.".ROOT_EX);
-		break;
-		case "smiles":
-		case "js":
-			include_once(ROOT_PATH."core/pages/js.".ROOT_EX);
-		break;
-		case "css":
-			include_once(ROOT_PATH."core/pages/css.".ROOT_EX);
-		break;
-		case "add":
-		case "takeadd":
-			include_once(ROOT_PATH."core/pages/add.".ROOT_EX);
-		break;
-		case "cat":
-			include_once(ROOT_PATH."core/pages/cats.".ROOT_EX);
-		break;
-		case "feedback":
-			include_once(ROOT_PATH."core/pages/feedback.".ROOT_EX);
-		break;
-		case "edit":
-		case "takeedit":
-			include_once(ROOT_PATH."core/pages/edit.".ROOT_EX);
-		break;
-		case "delete":
-			include_once(ROOT_PATH."core/pages/delete.".ROOT_EX);
-		break;
-		case "hacker":
-			include_once(ROOT_PATH."core/pages/hacker.".ROOT_EX);
-		break;
-		case "ajax":
-			include_once(ROOT_PATH."core/pages/ajax.".ROOT_EX);
-		break;
-		case "reg":
-			include_once(ROOT_PATH."core/pages/reg.".ROOT_EX);
-		break;
-		case "login":
-			include_once(ROOT_PATH."core/pages/login.".ROOT_EX);
-		break;
-		case "user":
-			include_once(ROOT_PATH."core/pages/user.".ROOT_EX);
-		break;
-		case "config_player":
-			include_once(ROOT_PATH."core/pages/config_player.".ROOT_EX);
-		break;
-		case "playlist":
-			include_once(ROOT_PATH."core/pages/playlist.".ROOT_EX);
-		break;
-		case "player":
-			include_once(ROOT_PATH."core/pages/player.".ROOT_EX);
-		break;
-		case "player_style":
-			include_once(ROOT_PATH."core/pages/player_style.".ROOT_EX);
-		break;
-		case "watch":
-			include_once(ROOT_PATH."core/pages/video.".ROOT_EX);
-		break;
-		case "html5":
-			include_once(ROOT_PATH."core/pages/html5.".ROOT_EX);
-		break;
-		case "image_send":
-			include_once(ROOT_PATH."core/pages/image_send.".ROOT_EX);
-		break;
-		case "search":
-			include_once(ROOT_PATH."core/pages/search.".ROOT_EX);
-		break;
-		case "rss":
-			include_once(ROOT_PATH."core/pages/rss.".ROOT_EX);
-		break;
-		case "parser_video":
-			include_once(ROOT_PATH."core/pages/parser_video.".ROOT_EX);
 		break;
 		case "main":
 		default:
