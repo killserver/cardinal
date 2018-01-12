d6879f7098bac287af037b73c1f24a3daef3a5ae
killserver <killeserver@gmail.com>
2018-01-12 10:46:59 +0200

    update versions

--- core/media/config.global.php
+++ core/media/config.global.php
@@ -126,7 +126,7 @@ if(!defined("VERSION_ADMIN_STYLE")) {
 	define("VERSION_ADMIN_STYLE", "1.6");
 }
 if(!defined("INTVERSION")) {
-	define("INTVERSION", "80165");
+	define("INTVERSION", "80175");
 }
 if(!defined("DB_VERSION")) {
 	define("DB_VERSION", "6.5");
