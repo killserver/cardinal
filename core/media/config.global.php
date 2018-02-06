675701af78548834a616cf94e342e8292c945e43
killserver <killeserver@gmail.com>
2018-02-06 14:44:07 +0200

    subversion

--- core/media/config.global.php
+++ core/media/config.global.php
@@ -96,7 +96,7 @@ if(!defined("VERSION_ADMIN_STYLE")) {
 	define("VERSION_ADMIN_STYLE", "1.7");
 }
 if(!defined("INTVERSION")) {
-	define("INTVERSION", "80307");
+	define("INTVERSION", "80312");
 }
 if(!defined("DB_VERSION")) {
 	define("DB_VERSION", "6.5");
