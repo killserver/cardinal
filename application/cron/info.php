75567d927588f88d2311c6138a87320c074a3022
killserver <killeserver@gmail.com>
2018-04-13 18:18:53 +0300

    add check version

--- application/cron/info.php
+++ application/cron/info.php
@@ -19,6 +19,35 @@ class Info {
 		$parser->post(array("server" => str_replace("https", "http", config::Select('default_http_host')), "ip" => (isset($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : ""), "version" => (defined("VERSION") ? VERSION : ""), "online" => $online));
 		$parser->get();
 		unset($parser);
+		if(file_exists(PATH_MEDIA."engine-mail.lock") && !file_exists(PATH_MEDIA."engine-mail-done.lock")) {
+			$rev = false;
+			if(config::Select("speed_update")===true) {
+				$parser = new Parser("https://raw.githubusercontent.com/killserver/cardinal/trunk/version/intversion.txt");
+				$parser->timeout(3);
+				$parser = $parser->get();
+				$rev = true;
+			} else {
+				$parser = new Parser("https://raw.githubusercontent.com/killserver/cardinal/trunk/version/version.txt");
+				$parser->timeout(3);
+				$parser = $parser->get();
+			}
+			if(cardinal_version($parser)) {
+				if($rev) {
+					$version = new Parser("https://raw.githubusercontent.com/killserver/cardinal/trunk/version/version.txt");
+					$version->timeout(3);
+					$version = $version->get();
+				}
+				$res = false;
+				try {
+					$mail = file_get_contents(PATH_MEDIA."engine-mail.lock");
+					$res = nmail($mail, "Появилось обновление движка <b>Cardinal Engine</b>.<br>\nУ Вас стоит версия <b>".VERSION."</b>".($rev ? " rev. <b>".INTVERSION."</b>" : "")."<br>\nНовая версия: <b>".($rev ? $version : $parser)."</b>".($rev ? " rev. <b>".$parser."</b>" : ""), "Cardinal Engine [quick-install]");
+				} catch(Exception $ex) {}
+				if($res) {
+					file_put_contents(PATH_MEDIA."engine-mail-done.lock", "");
+				}
+			}
+
+		}
 	}
 	
 }
