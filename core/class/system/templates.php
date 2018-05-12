00c303fa3f7796f19cf7be12b9d9ab0659f505e1
killserver <killeserver@gmail.com>
2018-05-12 22:20:48 +0300

    fix's

--- core/class/system/templates.php
+++ core/class/system/templates.php
@@ -242,6 +242,28 @@ class templates {
 		return microtime(true);
 	}
 
+	final private static function gen_uuid() {
+		return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+		// 32 bits for "time_low"
+		mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+
+		// 16 bits for "time_mid"
+		mt_rand(0, 0xffff),
+
+		// 16 bits for "time_hi_and_version",
+		// four most significant bits holds version number 4
+		mt_rand(0, 0x0fff) | 0x4000,
+
+		// 16 bits, 8 bits for "clk_seq_hi_res",
+		// 8 bits for "clk_seq_low",
+		// two most significant bits holds zero and one for variant DCE1.1
+		mt_rand(0, 0x3fff) | 0x8000,
+
+		// 48 bits for "node"
+		mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+		);
+	}
+
 	/**
 	 * Set array datas for template
 	 * @access public
@@ -251,7 +273,7 @@ class templates {
      */
 	final public static function assign_vars($array, $block = "", $view = "") {
 		if($view==="") {
-			$view = uniqid();
+			$view = self::gen_uuid();
 		}
 		if(empty($block)) {
 			foreach($array as $name => $value) {
