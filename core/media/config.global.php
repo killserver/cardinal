5c351e9a679d6a356ccc29cba7be4371192e392b
killserver <killeserver@gmail.com>
2018-07-11 13:03:01 +0300

    fix's

--- core/media/config.global.php
+++ core/media/config.global.php
@@ -96,7 +96,7 @@ if(!defined("VERSION_ADMIN_STYLE")) {
 	define("VERSION_ADMIN_STYLE", "1.9");
 }
 if(!defined("INTVERSION")) {
-	define("INTVERSION", "102015");
+	define("INTVERSION", "102020");
 }
 if(!defined("DB_VERSION")) {
 	define("DB_VERSION", "6.5");
